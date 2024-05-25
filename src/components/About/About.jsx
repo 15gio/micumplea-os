ff9ff74b6b21cb30d8502dfdf804789b37cc6f19
FJCon <conilfernando@gmail.com>
2024-05-25 16:27:14 -0300

    cards pendientes agregadas

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,6 +10,25 @@ const About = () => {
             title='lugar' 
             description='Salon Madison.'
             subdescription='Av. Richieri 3649, Santo Tomé.'
+            buttonText="Ver ubicación"
+        />
+        <Card 
+            title='Fecha' 
+            description='Sábado 3 de agosto'
+            subdescription='21:00 hs'
+            buttonText="Agendar"
+        />
+        <Card 
+            title='Vestimenta' 
+            description='Formal'
+            subdescription=''
+            buttonText=''
+        />
+        <Card 
+            title='¿Venís?' 
+            description='Me  gustaria compartir mi fiesta con vos'
+            subdescription=''
+            buttonText="Confirmar asistencia"
         />
     </div>
   )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({title, description, subdescription}) => {
+const Card = ({title, description, subdescription, buttonText}) => {
   return (
     <div className='card-container'>
         <h3>{title}</h3>
         <p>{description}</p>
         <p>{subdescription}</p>
-        <button>more info</button>
+        <button>{buttonText}</button>
     </div>
   )
 }
