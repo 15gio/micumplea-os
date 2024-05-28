4f145260bd34d8cd0a63096eb31cebaf00562621
FJCon <conilfernando@gmail.com>
2024-05-27 21:57:59 -0300

    Modificacion en portada

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -5,8 +5,12 @@ import './Portada.css'
 const Portada = () => {
   return (
     <div className='cover-container'>
+      <div className='text-container'>
+        <h3>Huilén</h3>
         <h1>Mis 15 años</h1>
-        <CuentaRegresiva />
+      </div>
+        
+      <CuentaRegresiva />
     </div>
   )
 }
